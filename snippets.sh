62ad6f9d0010066bfb6e1008ca4306f9dce10b44
Oscar Mier <voscarmv@gmail.com>
2022-07-20 22:55:42 -0500

    First commit

---
+++ snippets.sh
@@ -0,0 +1,34 @@
+CTR=0
+cat companies.last.txt | while read line ; do
+    URL="https://ycombinator.com$line"
+    wget -O temp $URL
+    COMPANYSITE=`cat temp |\
+        sed 's/M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z/&\n/' | grep -A1 'M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z' |\
+        tail -n 1 |\
+        sed 's/<\/a>.*//' |\
+        sed 's/.*>//'` 
+    echo $COMPANYSITE >> urls.txt
+    wget -O site $COMPANYSITE
+    GITHUB=`cat site | grep -o 'github.com/[^"][^"]*"' | sort | uniq | paste -sd ","` 
+    echo $COMPANYSITE $GITHUB >> githubs.txt
+    # if test $CTR -ge 10 ; then
+    #     break
+    # fi
+    # CTR=`echo $CTR+1 | bc`
+    sleep 1
+done
+
+# cat urls.txt | while read line ; do
+#     URL=$line
+#     wget -O site $URL
+#     cat site | grep -o 'github.com/[^"][^"]*"' | sort | uniq >> githubs.txt
+# done
+
+# cat zepto |\
+#     sed 's/M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z/&\n/' | grep -A1 'M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z' |\
+#     tail -n 1 |\
+#     sed 's/<\/a>.*//' |\
+#     sed 's/.*>//'
+
+# cat githubs.txt | grep -o "github.com/[-A-Za-z_/]*" | grep -v 'normalize' | sort | uniq | nl > githubrepo.txt
+
