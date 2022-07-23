a580eaee90a047abbca3711a6107629001d75db8
Oscar Mier <voscarmv@gmail.com>
2022-07-22 20:34:47 -0500

    Scraping using proper YC and GH APIs

--- scraper.js
+++ scraper.js
@@ -6,23 +6,33 @@ import fetch from 'node-fetch';
         let batch = batches[i];
         let page = await fetch(`https://api.ycombinator.com/v0.1/companies?batch=${batch}`);
         let pageJson = await page.json();
-        // console.log(`Batch ${batch} Page ${pageJson.page+1} of ${pageJson.totalPages}`);
-        pageJson.companies.forEach((company) => {
-            const html = await fetch(company.website);
-            const html_text = await html.text();
-            const githubs = html_text.match(/github.com\/[A-Za-z0-9_.-/]+/g);
-            console.log(`${company.name}, ${company.website}, ${company.url}, [${githubs.toString()}]`);
-        });
-        while(pageJson.nextPage !== undefined){
-            page = await fetch(pageJson.nextPage);
-            pageJson = await page.json();
-            // console.log(`Batch ${batch} Page ${pageJson.page+1} of ${pageJson.totalPages}`);
-            pageJson.companies.forEach((company) => {
+        console.log(`Batch ${batch} Page ${pageJson.page+1} of ${pageJson.totalPages}`);
+        for(let j = 0; j < pageJson.companies.length; j++) {
+            const company = pageJson.companies[j];
+            try{
                 const html = await fetch(company.website);
                 const html_text = await html.text();
                 const githubs = html_text.match(/github.com\/[A-Za-z0-9_.-/]+/g);
-                console.log(`${company.name}, ${company.website}, ${company.url}, [${githubs.toString()}]`);
-                });
+                console.log(`${company.name}, ${company.website}, ${company.url}, [${githubs ? githubs.toString().replace(",","|") : ''}]`);    
+            } catch(e) {
+                console.log(`${company.name}, ${company.website} #SITEDOWN, ${company.url}, []`);
+            }
+        }
+        while(pageJson.nextPage !== undefined){
+            page = await fetch(pageJson.nextPage);
+            pageJson = await page.json();
+            console.log(`Batch ${batch} Page ${pageJson.page+1} of ${pageJson.totalPages}`);
+            for(let j = 0; j < pageJson.companies.length; j++) {
+                const company = pageJson.companies[j];
+                try{
+                    const html = await fetch(company.website);
+                    const html_text = await html.text();
+                    const githubs = html_text.match(/github.com\/[A-Za-z0-9_.-/]+/g);
+                    console.log(`${company.name}, ${company.website}, ${company.url}, [${githubs ? githubs.toString().replace(",","|") : ''}]`);    
+                } catch(e) {
+                    console.log(`${company.name}, ${company.website} #SITEDOWN, ${company.url}, []`);
+                }
+            }
         }
     }
 })();
